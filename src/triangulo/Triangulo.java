e76a529c54486c543f42fb0c11e50b4eb3fd60dd
David <David@LAPTOP-VSEKM5I2>
2018-02-26 16:29:12 +0000

    COMMIT GITHUB.

---
+++ src/triangulo/Triangulo.java
@@ -0,0 +1,12 @@
+
+package triangulo;
+
+/**
+ *
+ * @author dfernandezguerreiro
+ */
+public class Triangulo {
+    public static void areaTriangulo(float base,float altura){
+        System.out.println("Area triangulo = "+base*altura/2);
+    }
+}
