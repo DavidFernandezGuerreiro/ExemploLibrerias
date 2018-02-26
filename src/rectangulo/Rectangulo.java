b320f6dd38c4f1f17810bd6d6ab13f018878311c
David <David@LAPTOP-VSEKM5I2>
2018-02-26 16:29:07 +0000

    COMMIT GITHUB.

---
+++ src/rectangulo/Rectangulo.java
@@ -0,0 +1,12 @@
+
+package rectangulo;
+
+/**
+ *
+ * @author dfernandezguerreiro
+ */
+public class Rectangulo {
+    public static void areaRectangulo(float base,float altura){
+        System.out.println("Area rectangulo = "+base*altura);
+    }
+}
